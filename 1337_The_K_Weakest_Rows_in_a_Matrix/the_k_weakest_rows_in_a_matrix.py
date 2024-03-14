681e6b041fd00f66aa8134f65b2660a3f0301d54
yatomiyoshi <seno.yatomi.0407@gmail.com>
2024-03-14 22:36:40 +0900

    add code

--- 1337_The_K_Weakest_Rows_in_a_Matrix/the_k_weakest_rows_in_a_matrix.py
+++ 1337_The_K_Weakest_Rows_in_a_Matrix/the_k_weakest_rows_in_a_matrix.py
@@ -5,3 +5,24 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        first = mat[0]
+        first_soldiers = sum(first)
+
+        weakest_rows_indices = []
+        weakest_rows_indices.append(0)
+
+        rows_and_soldiers = {}
+
+        if k == 1:
+            return weakest_rows_indices
+        
+        if len(mat) == 1:
+            return weakest_rows_indices
+
+        rows_and_soldiers[0] = first_soldiers
+
+        second = mat[1]
+        second_soldiers = sum(second)
+
+        for k, v in rows_and_soldiers.items():
+            
