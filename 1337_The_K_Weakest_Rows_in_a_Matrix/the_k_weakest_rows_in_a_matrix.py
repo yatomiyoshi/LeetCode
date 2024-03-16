e142d2890d201ec16242f70d89137f00f22b956f
yatomiyoshi <seno.yatomi.0407@gmail.com>
2024-03-16 22:40:53 +0900

    Add for loop

--- 1337_The_K_Weakest_Rows_in_a_Matrix/the_k_weakest_rows_in_a_matrix.py
+++ 1337_The_K_Weakest_Rows_in_a_Matrix/the_k_weakest_rows_in_a_matrix.py
@@ -24,10 +24,12 @@ class Solution(object):
         second = mat[1]
         second_soldiers = sum(second)
         
-        for i, member in enumerate(rows_and_soldiers):
-            if member[1] > second_soldiers:
-                if len(rows_and_soldiers) < k:
+        for j in range(len(mat)):
+            for i, member in enumerate(rows_and_soldiers):
+                if member[1] > second_soldiers:
                     rows_and_soldiers.insert(i, (1, second_soldiers))
-        else:
-            rows_and_soldiers.append((1, second_soldiers))
+                    if len(rows_and_soldiers) > k:
+                        rows_and_soldiers.pop(-1)
+            else:
+                rows_and_soldiers.append((1, second_soldiers))
         
