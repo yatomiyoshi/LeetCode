af0d4e8147062788cf1c03fcb3eae99b07f7b754
yatomiyoshi <seno.yatomi.0407@gmail.com>
2024-03-17 21:28:18 +0900

    Refactoring

--- 1337_The_K_Weakest_Rows_in_a_Matrix/the_k_weakest_rows_in_a_matrix.py
+++ 1337_The_K_Weakest_Rows_in_a_Matrix/the_k_weakest_rows_in_a_matrix.py
@@ -5,31 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        first = mat[0]
-        first_soldiers = sum(first)
-
-        weakest_rows_indices = []
-        weakest_rows_indices.append(0)
-
+        
         rows_and_soldiers = []
-
-        if k == 1:
-            return weakest_rows_indices
-        
-        if len(mat) == 1:
-            return weakest_rows_indices
-
-        rows_and_soldiers[0] = (0, first_soldiers)
-
-        second = mat[1]
-        second_soldiers = sum(second)
-        
-        for j in range(len(mat)):
-            for i, member in enumerate(rows_and_soldiers):
-                if member[1] > second_soldiers:
-                    rows_and_soldiers.insert(i, (1, second_soldiers))
-                    if len(rows_and_soldiers) > k:
-                        rows_and_soldiers.pop(-1)
+        for i in range(len(mat)):
+            soldiers = sum(mat[i])
+            for j, member in enumerate(rows_and_soldiers):
+                if soldiers < member[1]:
+                    rows_and_soldiers.insert(j, (i, soldiers))
+                    break
             else:
-                rows_and_soldiers.append((1, second_soldiers))
-        
+                rows_and_soldiers.append((i, soldiers))
