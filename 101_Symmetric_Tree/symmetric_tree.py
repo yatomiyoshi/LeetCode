50d164629a832bbf8d1dae7cdef3cdd1078a1460
yatomiyoshi <seno.yatomi.0407@gmail.com>
2024-03-29 21:38:31 +0900

    Add method

--- 101_Symmetric_Tree/symmetric_tree.py
+++ 101_Symmetric_Tree/symmetric_tree.py
@@ -10,3 +10,27 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
+        if not root.left and not root.right:
+            return True
+        if root.left and not root.right:
+            return False
+        if not root.left and root.right:
+            return False
+        if root.left.val != root.right.val:
+            return False
+        self.check(root.left, root.right)
+        return True
+
+    def check(self, right_node, left_node):
+        if right_node.val != left_node.val:
+            return False
+        if right_node.left and not left_node.right:
+            return False
+        if not right_node.left and not left_node.right:
+            return False
+        if right_node.right and not left_node.left:
+            return False
+        if not right_node.right and left_node.left:
+            return False
+        self.check(right_node.left, left_node.right)
+        self.check(right_node.right, left_node.left)
